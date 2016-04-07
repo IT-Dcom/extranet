6b4a9aca1719108c9073a5aa4251065c938d3b5d
Lairan <a.lairan@linkbynet.com>
2016-04-07 18:35:11 +0200

    Update footer.php

--- fuel/app/views/layouts/footer.php
+++ fuel/app/views/layouts/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer navbar-fixed-bottom navbar-inverse">
     <div class="row">
       <div class="col-xs-6">
-        <p>&copy; Copyright <?php echo date('Y');?> Livogs <?php echo $title; ?></p>
+        <p>&copy; Copyright <?php echo date('Y');?> Livogis <?php echo $title; ?></p>
       </div>
       <div class="col-xs-6">
         Nous contacter
