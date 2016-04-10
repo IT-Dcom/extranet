f8c557d381b052bde9e3d6a16367de60c294fde9
Alexandre Lairan <lairana@free.fr>
2016-04-10 10:28:24 +0200

    add font awesome

--- fuel/app/views/user/profil.php
+++ fuel/app/views/user/profil.php
@@ -1,14 +1,32 @@
-<div class="row">
-  <div class="col-sm-6">
-    <div class="form-group">
-      <input class="input-group date" value="Lairan Alexandre">
-        <span class="input-group-addon">
-          <span class="glyphicon glyphicon-user"></span>
+<div class='row'>
+  <div class='col-sm-6'>
+    <div class='input-group'>
+      <input name='username' class='form-control'
+             value='Lairan Alexandre' disabled='true'>
+        <span class='input-group-addon'>
+          <span class='glyphicon glyphicon-user'></span>
         </span>
       </input>
     </div>
   </div>
-  <div class="col-sm-6">
-
+  <div class='col-sm-6'>
+    <div class='input-group'>
+      <input name='email' class='form-control'
+             value='lairana@free.fr' disabled='true'>
+        <span class='input-group-addon'>
+          <span class='glyphicon glyphicon-envelope'></span>
+        </span>
+      </input>
+    </div>
+  </div>
+  <div class='col-sm-6'>
+    <div class='input-group'>
+      <input name='password' class='form-control' type='password'
+             value='mon_password' disabled='true'>
+        <span class='input-group-addon'>
+          <span class='glyphicon'><i class='fa fa-key'></i></span>
+        </span>
+      </input>
+    </div>
   </div>
 </div>
