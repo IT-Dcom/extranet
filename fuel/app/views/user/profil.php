080b394e7e06d3e59d5e18756fd44c44e64b51db
Alexandre Lairan <lairana@free.fr>
2016-04-10 11:19:26 +0200

    Add button and address for user page

--- fuel/app/views/user/profil.php
+++ fuel/app/views/user/profil.php
@@ -1,4 +1,4 @@
-<div class='row'>
+<div class='row livogis-form-espacement'>
   <div class='col-sm-6'>
     <div class='input-group'>
       <input name='username' class='form-control'
@@ -28,5 +28,39 @@
         </span>
       </input>
     </div>
+  </div>
+  <div class='col-sm-6'>
+    <div class='input-group'>
+      <input name='addresse' class='form-control'
+             value='12 allée des pinsons' disabled='true'>
+        <span class='input-group-addon'>
+          <span class='glyphicon glyphicon-home'></span>
+        </span>
+      </input>
+    </div>
+  </div>
+  <div class='col-sm-6'>
+    <div class='input-group'>
+      <input name='addresse' class='form-control'
+             value='Saint Pathus' disabled='true'>
+        <span class='input-group-addon'>
+          <span class='glyphicon'><i class="fa fa-map-marker"></i></span>
+        </span>
+      </input>
+    </div>
+  </div>
+  <div class='col-sm-6'>
+    <div class='input-group'>
+      <input name='addresse' class='form-control'
+             value='77178' disabled='true'>
+        <span class='input-group-addon'>
+          <span class='glyphicon'>ZIP</span>
+        </span>
+      </input>
+    </div>
+  </div>
+
+  <div class="col-sm-3" style="margin-left: 10px;margin-top: 5px;">
+    <input type="submit" name="Modifier" value="Modifier" class="btn btn-success">
   </div>
 </div>
