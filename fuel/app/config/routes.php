004deab373206f546f631e73228548944a15413e
alex-lairan <a.lairan@linkbynet.com>
2016-04-11 09:32:52 +0200

    Add comma

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -3,7 +3,7 @@ return array(
 	'_root_'  => 'home/index',  // The default route
 
 	'colis/new' => 'colis/create',
-	'colis'  => 'colis/index'
+	'colis'  => 'colis/index',
 
 	'user/profil' => 'user/profil',
 	// exemple: 'index(/:name)?' => array('home/index'),
